7b88cc60ef91833fd903d07e367cbf949d9bd622
Xacrys10 <Xacrys10@DESKTOP-0C76TBU>
2018-05-12 13:25:34 -0500

    clases de configuracion

---
+++ config/baseDeDatos.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    "driver"    =>"mysql",
+    "host"      =>"localhost",
+    "user"      =>"root",
+    "pass"      =>"",
+    "database"  =>"nanomarket",
+    "charset"   =>"utf8",
+);
+
+?>
+
