ec14a8b8e700e48dfa8b45025b1d16c68029f209
Xacrys10 <Xacrys10@DESKTOP-0C76TBU>
2018-05-12 13:34:43 -0500

    clases de configuracion

--- core/Conectar.php
+++ core/Conectar.php
@@ -5,4 +5,27 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+class Conectar{
+    private $driver,$host,$user,$pass,$database,$charset; 
+    
+    public function construct(){
+        $db_cfg = require_once 'config/baseDeDatos.php';
+        $this->driver = $db_cfg["driver"];
+        $this->host = $db_cfg["host"];
+        $this->user = $db_cfg["user"];
+        $this->pass = $db_cfg["pass"];
+        $this->database = $db_cfg["database"];
+        $this->charset = $db_cfg["charset"];
+        
+    }
+    
+    public function conexion(){
+        
+        if($this->driver=="mysql" || $this->driver==null){
+            $con = new mysqli($this->host,$this->user, $this->pass, $this->database);
+            $con->query("SET NAMES '".$this->charset."'");
+        }
+        return $con;
+    }
+}
+?>
