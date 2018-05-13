d99292c046f9ad23aa8b7fc4c41050308c0ad05d
unknown <geovannyadq@gmail.com>
2018-05-13 14:34:26 -0500

    Categoria Modelo

---
+++ modelo/CategoriaModelo.php
@@ -0,0 +1,41 @@
+<?php
+
+class CategoriaModelo extends ModeloBase{
+    private $idcategoria, $nombre, $descripcion, $activo;
+    
+    
+    public function __construct() {
+        $tabla="categoria";
+        parent::__construct($tabla);
+    }
+
+    //getters
+    public function getIdcategoria() {
+        return $this->idcategoria;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getDescripcion() {
+        return $this->descripcion;
+    }    
+
+    //setters
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function setDescripcion($descripcion) {
+        $this->descripcion = $descripcion;
+    }
+
+    public function guardar(){
+        $query="INSERT INTO categoria (nombre, descripcion) VALUES (
+            '".$this->nombre."',
+            '".$this->descripcion."'            
+            )"; 
+        $this->db()->query($query);        
+    }
+}
